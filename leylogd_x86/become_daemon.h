058ceab70d3053f934c7d204784b8d856b30601d
ChristopherLey <christopher.ley@uon.edu.au>
2015-03-02 13:48:20 -0300

    1st commit leylogd_x86

---
+++ leylogd_x86/become_daemon.h
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : become_daemon.h
+// Author      : Christopher Ley
+// Version     : 1.0
+// Project	   : leylogd
+// Created     : 24/02/15
+// Modified    : 24/02/15
+// Copyright   : Do not modify without express permission from the author
+// Description : Daemon process creation header file
+//===========================================================================
+#ifndef BECOME_DAEMON_H	/* Prevent double inclusion */
+#define BECOME_DAEMON_H
+
+
+/* Bit-mask values for 'flags' argument of becomeDaemon() */
+#define BD_NO_CHDIR				01		/* Don't chdir("/") */
+#define BD_NO_CLOSE_FILES		02		/* Don't close all open files */
+#define BD_NO_REOPEN_STD_FDS	04		/* Don't reopen stdin, stdout, and stderr to /dev/null */
+#define BD_NO_UMASK0			010		/* Don't do a umask(0) */
+#define BD_MAX_CLOSE			8192	/* Maximum file descriptors to close if sysconf(_SC_OPEN_MAX) is indeterminate */
+
+int becomeDaemon(int flags);
+
+#endif
